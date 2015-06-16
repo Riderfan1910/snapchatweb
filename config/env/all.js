68da83396a0f3dc9e439fcd31f4e2b350c735b4e
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-06-15 23:44:35 -0400

    javascript works

--- config/env/all.js
+++ config/env/all.js
@@ -15,13 +15,32 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+                '',
+                ''
 			],
 			js: [
 				'public/lib/angular/angular.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+                'public/lib/js/jquery-1.11.2.min.js',
+                'public/lib/js/jquery.bxslider.min.js',
+                'public/lib/js/jquery.easing.1.3.js',
+                'public/lib/js/jquery.fitvids.js',
+                'public/lib/js/jquery.placeholder.js',
+                'public/lib/js/jquery.touchSwipe.js',
+                'public/lib/js/jquery.validate.min.js',
+                'public/lib/js/modernizr.custom.js',
+                'public/lib/js/bootstrap.min.js',
+                'public/lib/js/chart.js',
+                'public/lib/js/color-switcher.js',
+                'public/lib/js/gallery.js',
+                'public/lib/js/icheck.min.js',
+                'public/lib/js/mailer.js',
+                'public/lib/js/smoothscroll.js',
+                'public/lib/js/waypoints.min.js',
+                'public/lib/js/landing2.js'
 			]
 		},
 		css: [
